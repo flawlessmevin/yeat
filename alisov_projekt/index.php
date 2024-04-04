2738d176cc598b0ae35de914bf56565808cb66c6
flawlessmevin <vanyaalisov10@gmail.com>
2024-04-04 15:46:34 +0200

    include footer

--- alisov_projekt/index.php
+++ alisov_projekt/index.php
@@ -24,9 +24,9 @@
 	<nav class="navigation">
 		<ul>
 			<li><a href="index.php">Home</a></li>
-			<li><a href="./music.html">Music</a></li>
-			<li><a href="./video.html">Video</a></li>
-			<li><a href="./tour.html">Tour</a></li>
+			<li><a href="music.php">Music</a></li>
+			<li><a href="video.php">Video</a></li>
+			<li><a href="tour.php">Tour</a></li>
 		</ul>
 		<div class="hamburger" id="hamburger">
 			<div class="bar"></div>
@@ -255,60 +255,7 @@
 	</div>
 </main>
 
-<footer>
-	<div class="container" id="footer-container">
-		<div class="row">
-
-			<div class="links-container col-md-4" style="text-align: center;">
-				<h3 style="color: white; margin-bottom: 20px;">Links</h3>
-				<div class="icon mb-3">
-
-					<a href="https://www.instagram.com/yeat/" target="_blank">
-						<img src="./img/instagram.png" alt="Instagram"> Instagram
-					</a>
-				</div>
-
-				<div class="icon mb-3" style="margin-left: -23px;">
-					<a href="https://open.spotify.com/artist/3qiHUAX7zY4Qnjx8TNUzVx" target="_blank">
-						<img src="./img/spotify.png" alt="Spotify"> Spotify
-					</a>
-				</div>
-				<div class="icon" style="margin-left: -10px;">
-					<a href="https://www.youtube.com/@yeatmusic3280/featured" target="_blank">
-						<img src="./img/youtube.png" alt="YouTube"> YouTube
-					</a>
-				</div>
-			</div>
-
-
-			<div class="contact-container col-md-4" style="text-align: center;">
-				<h3 style="color: white; margin-bottom: 20px;">Contact Us</h3>
-				<div class="contact-details">
-					<p>Phone: <a href="tel:+17472310628">+1(747)231-0628</a></p>
-					<p>Mngmt: <a href="tel:+13455022093">+1(345)502-2093</a></p>
-					<p>Email: <a href="mailto:yeatmgmt17@gmail.com">yeatmgmt17@gmail.com</a></p>
-					<p>Address: Portland, Oregon, U.S.</p>
-				</div>
-			</div>
-
-
-			<div class="navigation-container col-md-4" style="text-align: center;">
-				<h3 style="color: white; margin-bottom: 20px;">Navigation</h3>
-				<div class="contact-details">
-					<p><a href="index.php">Home</a></p>
-					<p><a href="./music.html">Music</a></p>
-					<p><a href="./video.html">Video</a></p>
-					<p><a href="./tour.html">Tour</a></p>
-				</div>
-			</div>
-		</div>
-		<div class="row copyright-row">
-			<div class="col-md-12">
-				&copy; 2023 Ivan ALisov. All rights reserved.
-			</div>
-		</div>
-	</div>
-</footer>
+<?php include "parts/footer.php"?>
 
 
 
---
+++ alisov_projekt/tour.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Music</title>
+  <link rel="icon" href="./img/yeat icon.png" type="image/x-icon">
+  <link rel="shortcut icon" href="./img/yeat icon.png " type="image/x-icon">
+
+  <link rel="stylesheet" href="./css/style.css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+</head>
+
+
+
+<body>
+  <header class="banner">
+    <div class="logo">
+      <a href="index.php">
+        <img src="./img/yeat-twizzy gif.gif" height="40">
+      </a>
+    </div>
+    <nav class="navigation">
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="music.php">Music</a></li>
+        <li><a href="video.php">Video</a></li>
+        <li><a href="tour.php">Tour</a></li>
+      </ul>
+      <a class="hamburger" id="hamburger">
+        <i class="fa fa-bars"></i>
+      </a>
+    </nav>
+  </header>
+  <main>
+    <div class="image-container">
+      <img src="./img/yeat 4 full width.jpg" alt="Full Width Image">
+      <div class="text-overlay">
+        <p class="glow-text" style="font-size: 150px;">CONCERTS</p>
+      </div>
+    </div>
+
+
+    <div class="rozdel">
+      <h2>LIVE PERFOMANCES</h2>
+    </div>
+
+
+
+    <section class="slides-container">
+      <div class="slide fade">
+        <img src="./img/yeat4.jpeg" style="max-width: 1500px; max-height: 1000px;">
+        <div class="slide-text">
+          New York
+        </div>
+      </div>
+      <div class="slide fade">
+        <img src="./img/live yeat2.jpg" style="max-width: 1500px; max-height: 1000px;">
+        <div class="slide-text">
+          California
+        </div>
+      </div>
+      <div class="slide fade">
+        <img src="./img/live yeat3.jpg">
+        <div class="slide-text">
+          London
+        </div>
+      </div>
+      <a id="prev" class="prev">❮</a>
+      <a id="next" class="next">❯</a>
+    </section>
+
+
+
+    <div class="container-sm mt-5" id="albums">
+      <div class="row justify-content-around custom-mb">
+        <div class="col-md-5">
+          <h2 class="big-h2 glow-text">
+            There are no events in the near future. Sign up to receive information.
+          </h2>
+
+        </div>
+        <div class="col-md-5">
+          <form id="contactForm" onsubmit="return validateForm()">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required>
+
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+
+            <label for="message">Message:</label>
+            <textarea id="message" name="message" rows="4" required></textarea>
+
+            <label>
+              <input type="checkbox" id="consent" name="consent" required>
+              I consent to the processing of personal data.
+            </label>
+
+            <button type="submit"><a href="https://en.wikipedia.org/wiki/Yeat"></a>Send</button>
+          </form>
+
+        </div>
+      </div>
+
+
+
+
+  </main>
+
+
+  <?php include "parts/footer.php"?>
+
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script src="./js/app.js"></script>
+  <script src="./js/hamb.js"></script>
+</body>
+
+</html>
