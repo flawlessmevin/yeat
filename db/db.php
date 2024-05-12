528ac8ba0aa69f4f9432259f754c8fa9e88d0ac5
flawlessmevin <vanyaalisov10@gmail.com>
2024-05-12 13:38:01 +0200

    database singles & albums

--- db/db.php
+++ db/db.php
@@ -1,3 +1,22 @@
+<?php
+
+$dbhost = "localhost";
+$dbname = "yeat";
+$username = "root";
+$password = "";
+
+$db = new PDO("mysql:host=$dbhost; dbname=$dbname", $username, $password);
+
+function get_albums() {
+    global $db;
+    $albums = $db->query("SELECT * FROM albums");
+    return $albums;
+}
 
 
+function get_singles() {
+    global $db;
+    $singles = $db->query("SELECT * FROM singles");
+    return $singles;
+}
 
--- music.php
+++ music.php
@@ -27,237 +27,52 @@
     <div class="rozdel">
       <h2>Albums</h2>
     </div>
-    <div class="container-sm mt-5" id="albums">
-      <div class="row justify-content-around custom-mb">
-        <div class="col-md-5">
-          <iframe class="spotify-player"
-            src="https://open.spotify.com/embed/album/1n3e9mGQUpv1UXOxMre4My?utm_source=generator" width="100%"
-            height="352" frameBorder="0" allowfullscreen=""
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-        </div>
-        <div class="col-md-5">
-          <h2 class="big-h2 glow-text">Alivё</h2>
-          <h3 class="small-h3">Released April 2, 2021</h3>
-          <p class="small-p">Alivë is Yeat’s third mixtape and his first release of
-            2021.
-            The project was highly anticipated by Yeat’s fans as there was a long wait for the project due to label
-            troubles Yeat
-            was experiencing, and constant previews.</p>
-        </div>
+
+      <?php
+
+      $albums = get_albums();
+      foreach ($albums as $album){?>
+      <div class="container-sm mt-5" id="albums">
+          <div class="row justify-content-around custom-mb">
+              <div class="col-md-5">
+                  <iframe class="spotify-player"
+                          src="<?php echo $album["spotify_link"]?>" width="100%"
+                          height="352" frameBorder="0" allowfullscreen=""
+                          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+              </div>
+              <div class="col-md-5">
+                  <h2 class="big-h2 glow-text"><?php echo $album["title"]?></h2>
+                  <h3 class="small-h3"><?php echo $album["date"]?></h3>
+                  <p class="small-p"><?php echo $album["description"]?></p>
+              </div>
+          </div>
       </div>
-
-
-
-      <div class="row justify-content-around custom-mb">
-        <div class="col-md-5">
-          <iframe class="spotify-player"
-            src="https://open.spotify.com/embed/album/26LsyqIbRQJzO1TPE5icl2?utm_source=generator" width="100%"
-            height="352" frameBorder="0" allowfullscreen=""
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-        </div>
-        <div class="col-md-5">
-          <h2 class="big-h2 glow-text">4L</h2>
-          <h3 class="small-h3">Released June 10, 2021</h3>
-          <p class="small-p">4L is Yeat’s studio album and the follow-up to his April 2021 project, Alivë.
-            The project gained traction when the track “Sorry Bout That” took off, accompanied by a music video and
-            TikTok
-            popularity.</p>
-        </div>
-      </div>
-
-      <div class="row justify-content-around custom-mb">
-        <div class="col-md-5">
-          <iframe class="spotify-player"
-            src="https://open.spotify.com/embed/album/3ZKEPJwhLcpUfYPMIsC4PF?utm_source=generator" width="100%"
-            height="352" frameBorder="0" allowfullscreen=""
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-        </div>
-        <div class="col-md-5">
-          <h2 class="big-h2 glow-text">Up to Mё</h2>
-          <h3 class="small-h3">Released September 10, 2021</h3>
-          <p class="small-p">Up 2 Më is Yeat’s third independently-released album of 2021 and his fourth release of
-            the
-            year, following
-            April’s Alivë, June’s 4L, and August’s Trëndi. As he explained in an interview with Our Generation Music,
-            the album’s title represents his control over his life and success:</p>
-        </div>
-      </div>
-
-      <div class="row justify-content-around custom-mb">
-        <div class="col-md-5">
-          <iframe class="spotify-player"
-            src="https://open.spotify.com/embed/album/0d1BFY8z15vye3KjtLvF3u?utm_source=generator" width="100%"
-            height="352" frameBorder="0" allowfullscreen=""
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-        </div>
-        <div class="col-md-5">
-          <h2 class="big-h2 glow-text">2 Alivё</h2>
-          <h3 class="small-h3">Released February 18, 2022</h3>
-          <p class="small-p">2 Alivë is the sequel to Yeat’s April 2021 project, Alivë, and his first album released
-            through Interscope
-            Records.
-            The album was first teased on December 13, 2021, where Yeat shared the project’s official cover art via
-            social
-            media,
-
-            teasing fans with a January 2022 release date, however the album missed the initial deadline.</p>
-
-        </div>
-      </div>
-      <div class="row justify-content-around custom-mb">
-        <div class="col-md-5">
-          <iframe class="spotify-player"
-            src="https://open.spotify.com/embed/album/0345WPzPBSeISh2IpIQWxT?utm_source=generator" width="100%"
-            height="352" frameBorder="0" allowfullscreen=""
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-        </div>
-        <div class="col-md-5">
-          <h2 class="big-h2 glow-text">2 Alivë (Geëk Pack)</h2>
-          <h3 class="small-h3">Released April 1, 2022</h3>
-          <p class="small-p">2 Alivë: Geëk Pack is the deluxe version of Yeat’s debut album, 2 Alivë.
-            On February 16, 2022, Yeat announced in an Instagram story that a deluxe version of 2 Alivë was on the
-            way.
-            The entire album was leaked two days before its official release, on March 30, 2022.</p>
-        </div>
-      </div>
-
-
-
-
-
-
-
-      <div class="row justify-content-around custom-mb">
-        <div class="col-md-5">
-          <iframe class="spotify-player"
-            src="https://open.spotify.com/embed/album/6Xo2PDEoQKzCndIbks2kvu?utm_source=generator" width="100%"
-            height="352" frameBorder="0" allowfullscreen=""
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-        </div>
-        <div class="col-md-5">
-          <h2 class="big-h2 glow-text">Lyfё</h2>
-          <h3 class="small-h3">Released September 9, 2022</h3>
-          <p class="small-p">Lyfë is Yeat’s fifth extended play, following his April 2022 deluxe album 2 Alivë (Geëk
-            Pack).
-            The project would first be revealed on June 15, 2022, in a now-deleted Tweet where Yeat confirmed
-            this EP would precede before the eponymous album.</p>
-        </div>
-      </div>
-
-
-      <div class="row justify-content-around custom-mb">
-        <div class="col-md-5">
-          <iframe class="spotify-player"
-            src="https://open.spotify.com/embed/album/25Uddgldy3slnChqKqHsIM?utm_source=generator" width="100%"
-            height="352" frameBorder="0" allowfullscreen=""
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-        </div>
-        <div class="col-md-5">
-          <h2 class="big-h2 glow-text">AftërLyfe</h2>
-          <h3 class="small-h3">Released February 24, 2023</h3>
-          <p class="small-p">AftërLyfe is Yeat’s third studio album, following his September 2022 extended play Lyfë.
-            The first mention of Aftërlyfe was on June 15, 2022, when Yeat confirmed in a now deleted
-            tweet that his EP, Lyfë, would release preceding this album..</p>
-        </div>
-      </div>
-
-
-        <div class="row justify-content-around custom-mb">
-            <div class="col-md-5">
-                <iframe class="spotify-player"
-                        src="https://open.spotify.com/embed/album/1jXYc5gip5tqCTDOotfY5L?utm_source=generator" width="100%"
-                        height="352" frameBorder="0" allowfullscreen=""
-                        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-            </div>
-            <div class="col-md-5">
-                <h2 class="big-h2 glow-text">2093</h2>
-                <h3 class="small-h3">Released February 16, 2024</h3>
-                <p class="small-p">AftërLyfe is Yeat’s third studio album, following his September 2022 extended play Lyfë.
-                    The first mention of Aftërlyfe was on June 15, 2022, when Yeat confirmed in a now deleted
-                    tweet that his EP, Lyfë, would release preceding this album..</p>
-            </div>
-        </div>
-    </div>
-
-
+          <?php }; ?>
 
 
     <div class="rozdel">
       <h2>Singles</h2>
     </div>
-    <div class="container-sm mt-5" id="albums">
-      <div class="row justify-content-around custom-mb">
-        <div class="col-md-5">
-          <iframe style="border-radius:12px"
-            src="https://open.spotify.com/embed/album/2JDTUHzxf0mkHdKxIw8Aje?utm_source=generator" width="100%"
-            height="352" frameBorder="0" allowfullscreen=""
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-        </div>
-        <div class="col-md-5">
-          <h2 class="big-h2 glow-text">Rich Minion</h2>
-          <h3 class="small-h3">Released June 28, 2022</h3>
-          <p class="small-p">“Rich Minion” is an ode to the major motion picture film, Minions: The Rise of Gru, by
-            Yeat.Throughout the track, Yeat
-            braggadociously raps about his wealth and affiliation with the Minions, expressing his hate for Despicable
-            Me’s
-            antagonist, Vector.</p>
-        </div>
-      </div>
+      <?php
 
-      <div class="row justify-content-around custom-mb">
-        <div class="col-md-5">
-          <iframe style="border-radius:12px"
-            src="https://open.spotify.com/embed/album/6K1GUtpgDr2wYCnKf1JOed?utm_source=generator" width="100%"
-            height="352" frameBorder="0" allowfullscreen=""
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-        </div>
-        <div class="col-md-5">
-          <h2 class="big-h2 glow-text">Already Rich</h2>
-          <h3 class="small-h3">Released May 3, 2023</h3>
-          <p class="small-p">“Already Rich” is a long-teased track by Yeat on which he uses several different flows
-            over an up-tempo beat.
+      $singles = get_singles();
+      foreach ($singles as $single){?>
+          <div class="row justify-content-around custom-mb">
+              <div class="col-md-5">
+                  <iframe style="border-radius:12px"
+                          src="<?php echo $single["spotify_link"]?>" width="100%"
+                          height="352" frameBorder="0" allowfullscreen=""
+                          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+              </div>
+              <div class="col-md-5">
+                  <h2 class="big-h2 glow-text"><?php echo $single["title"]?></h2>
+                  <h3 class="small-h3"><?php echo $single["date"]?></h3>
+                  <p class="small-p"><?php echo $single["description"]?></p>
+              </div>
+            </div>
+          </div>
 
-            This song was leaked during the summer of 2021, before the release of Yeat’s debut studio album Up 2 Me,
-            and quickly
-            gained popularity among fans.</p>
-        </div>
-
-      </div>
-      <div class="row justify-content-around custom-mb">
-        <div class="col-md-5">
-          <iframe style="border-radius:12px"
-            src="https://open.spotify.com/embed/album/05zSfoTPUX9IFb4AYbBaBz?utm_source=generator" width="100%"
-            height="352" frameBorder="0" allowfullscreen=""
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-        </div>
-        <div class="col-md-5">
-          <h2 class="big-h2 glow-text">My Wrist (feat. Young Thug)</h2>
-          <h3 class="small-h3">Released May 26, 2023</h3>
-          <p class="small-p">Yeat then posted on his Instagram story on May 23rd that “My Wrist” will release
-            Friday, May 26th, saying “###FRIDAY MY
-            WRIST W YOUNG THUG WHO READY??!……#####”.</p>
-        </div>
-      </div>
-
-      <div class="row justify-content-around custom-mb">
-        <div class="col-md-5">
-          <iframe style="border-radius:12px"
-            src="https://open.spotify.com/embed/album/5bVIz2j5nOioetMjM0T4F9?utm_source=generator" width="100%"
-            height="352" frameBorder="0" allowfullscreen=""
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-        </div>
-        <div class="col-md-5">
-          <h2 class="big-h2 glow-text">bigger thën everything</h2>
-          <h3 class="small-h3">Released August 10, 2023</h3>
-          <p class="small-p">“bigger thën everything” is the second lead single for Yeat’s upcoming album
-            following the Young Thug assisted release
-            “My wrist” in May 2023. The song features softer production from frequent producer BNYX®, similar to
-            songs like “Can’t
-            stop it” and “Mysëlf”.</p>
-        </div>
-      </div>
-    </div>
-
+      <?php }; ?>
 
 
   </main>
